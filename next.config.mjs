b5b065c10bc5de5cf9bc15767db2f762a2279ad3
DobromirK <kralev.dobromir@gmail.com>
2024-09-30 22:41:12 +0300

    Initial Commit NextJS

---
+++ next.config.mjs
@@ -0,0 +1,11 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  // Enable static export
+  // If the site is served from a sub-path, configure `basePath`
+  // Replace 'my-repo-name' with your repository name
+  //   basePath: "/dobromirkralev.github.io.git",
+  // Optional: If you want to handle trailing slashes in paths
+  //   trailingSlash: true,
+};
+
+export default nextConfig;
